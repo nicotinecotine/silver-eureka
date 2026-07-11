6e84fbfd1818432c586b3c9d4ec28732bfcdc01b
Nikita Belyaev <nikita.belyaev@touchin.ru>
2026-07-11 21:55:23 +0300

    aaaa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3206435846"
+    serviceWorkerVersion: "3554615671"
   }
 });
